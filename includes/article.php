2659e5a6b39338c8f71e8c1a7606584b8d8739b3
SpartaN-Coder-USSpartaN-Coder-USexitexitclear:qSpartaN-Coder-US <personaldandoci@gmail.com>
2024-09-22 15:32:59 +0300

    get the id result from the querry

--- includes/article.php
+++ includes/article.php
@@ -7,4 +7,20 @@ function getArticle($conn,$id)
             WHERE id = ?";
     
     $stmt = mysqli_prepare($conn, $sql);
+
+    if ($stmt === false) {
+
+        echo mysqli_error($conn);
+
+    } else { 
+
+        mysqli_stmt_bind_param($stmt, "i", $id);  
+
+        if (mysqli_stmt_execute($stmt))  {
+
+            $result = mysqli_stmt_get_result($stmt); 
+
+        }
+
+    }
 }
